849762c07adf862b668cee5de43aa37b3c441207
Toby Rumans <trumans@vmware.com>
2021-03-29 16:06:45 -0600

    Kotlin from spring initializr

---
+++ kotlin/src/main/kotlin/com/example/routeauthorizationtesting/RouteAuthorizationTestingApplication.kt
@@ -0,0 +1,11 @@
+package com.example.routeauthorizationtesting
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class RouteAuthorizationTestingApplication
+
+fun main(args: Array<String>) {
+	runApplication<RouteAuthorizationTestingApplication>(*args)
+}
